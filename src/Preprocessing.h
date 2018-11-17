c60acb04b98a91a3f9e4ba9ac7649a1a38c2a7db
Edward Seley <edwardseley@gmail.com>
2018-11-17 13:30:57 -0500

    Delete Preprocessing.h

--- src/Preprocessing.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-using namespace std;
-
-void windowSignal(vector <double> signal, int sampleCount);
-vector <double> downMix(double * signal, int sampleCount, int channels);
-void DCOffset(vector <double> signal, int sampleCount);
